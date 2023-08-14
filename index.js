a5df88bf1ee845f5301f65745ac9af2deb20e627
thanhnt <buitienthanhnt@gmail.com>
2023-08-15 01:19:35 +0700

    main: L-5 update code for firebase in-app-message.

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ import {AppRegistry, Linking} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import  messaging from "@react-native-firebase/messaging";
+import inAppMessaging from '@react-native-firebase/in-app-messaging';
+
+async function bootstrap() {
+	await inAppMessaging().setMessagesDisplaySuppressed(true);
+}
+
+bootstrap();
 
 messaging().setBackgroundMessageHandler(async message=>{
 	// navigation to screen
