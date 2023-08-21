bbfdf4b6db8d2246ff7b45ca94eaa4f4cc1451c9
buitienthanhnt@gmail.com <namtanvietnam>
2023-08-21 17:31:45 +0700

    main/category: Update code

--- screens/MoreScreen/SwipeListViews.js
+++ screens/MoreScreen/SwipeListViews.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   Image,
+  ScrollView,
+  RefreshControl
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -13,14 +15,12 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // npm i react-native-swipe-list-view
 import { SwipeListView } from 'react-native-swipe-list-view';
 
-// npm i react-native-elements
-// import { Icon } from 'react-native-elements';
-
 export default function SwipeListViews() {
 
+  const [resfresh, setRefresh] = useState(false);
   const [userData] = useState([
     {
-      img: 'https://randomuser.me/api/portraits/men/81.jpg',
+      img: 'http://192.168.100.210/newpaper/public/storage/files/shares/images up/14-1662952325-204-width740height493.jpg',
       name: 'Maurice Davis',
     },
     {
@@ -28,7 +28,7 @@ export default function SwipeListViews() {
       name: 'Bernice Alvarez',
     },
     {
-      img: 'https://randomuser.me/api/portraits/women/19.jpg',
+      img: 'http://192.168.100.210/newpaper/public/storage/files/shares/images up/14-1662952325-204-width740height493.jpg',
       name: 'Jennie Barnett',
     },
     {
@@ -40,17 +40,35 @@ export default function SwipeListViews() {
       name: 'Christian Wilson',
     },
     {
-      img: 'https://randomuser.me/api/portraits/women/21.jpg',
+      img: 'http://192.168.100.210/newpaper/public/storage/files/shares/images up/14-1662952325-204-width740height493.jpg',
       name: 'Sophia Fernandez',
     },
     {
-      img: 'https://randomuser.me/api/portraits/women/42.jpg',
-      name: 'Sylvia Lynch',
+      img: 'http://192.168.100.210/newpaper/public/storage/files/shares/images up/14-1662952325-204-width740height493.jpg',
+      name: 'Maurice Davis',
+    },
+    {
+      img: 'https://randomuser.me/api/portraits/women/20.jpg',
+      name: 'Bernice Alvarez',
+    },
+    {
+      img: 'http://192.168.100.210/newpaper/public/storage/files/shares/images up/14-1662952325-204-width740height493.jpg',
+      name: 'Jennie Barnett',
     },
   ]);
 
+  const onRefresh = useCallback(() => {
+    // alert("refresh");
+  })
+  
+
   return (
-    <View>
+    <ScrollView 
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      refreshControl={<RefreshControl refreshing={resfresh} onRefresh={onRefresh} />}
+      style={{flex:1, padding: 6}}
+    >           
       <SwipeListView
         data={userData}
         renderItem={(data, rowMap) => (
@@ -61,7 +79,6 @@ export default function SwipeListViews() {
           >
             <View
               style={{
-                backgroundColor: '#f1f3f6',
                 paddingVertical: 10,
                 paddingHorizontal: 10,
                 marginVertical: 4,
@@ -69,11 +86,12 @@ export default function SwipeListViews() {
                 display: 'flex',
                 flexDirection: 'row',
                 alignItems: 'center',
-                backgroundColor: "green"
+                backgroundColor: "#e37fff",
+               
               }}
             >
               <Image
-                style={{ width: 40, height: 40, borderRadius: 100 }}
+                style={{ width: 40, height: 40, borderRadius: 40, resizeMode: 'cover' }}
                 source={{ uri: data.item.img }}
               />
               <Text
@@ -89,53 +107,44 @@ export default function SwipeListViews() {
           </TouchableWithoutFeedback>
         )}
         renderHiddenItem={(data, rowMap) => (
-          <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-            <View
-              style={{
-                width: 75,
-                marginVertical: 4,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
+          <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingTop: 10 }}>
+            <View style={styles.hiddenItem} >
               <TouchableOpacity
                 onPress={() => {
-                  // Alert.alert('User Info', 'SwipeList Left Button');
+                 setRefresh(true)
                 }}
               >
-                <Icon name='bug' size={28} color='tomato' />
+                <Icon name='send' size={28} color='tomato'/>
               </TouchableOpacity>
             </View>
-            <View
-              style={{
-                width: 75,
-                height: 40,
-                marginVertical: 4,
-                display: 'flex',
-                justifyContent: 'center',
-                backgroundColor: "violet"
-              }}
-            >
+            <View style={styles.hiddenItem} >
               <TouchableOpacity
                 onPress={() => {
-                  // Alert.alert('Add User', 'SwipeList Right Button');
+                setRefresh(false)
                 }}
               >
-                <Icon name='bug' size={28} color='tomato' />
+                <Icon name='bug' size={28} color='#00ffca' />
               </TouchableOpacity>
             </View>
           </View>
         )}
-        leftOpenValue={75}
-        rightOpenValue={-75}
+        leftOpenValue={60}
+        rightOpenValue={-60}
       />
-
-      <Text>lklklklk</Text>
-    </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
 
+  },
+  hiddenItem: {
+    width: 50,
+    height: '100%',
+    marginVertical: 4,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
