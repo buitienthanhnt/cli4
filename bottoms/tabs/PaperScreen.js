22f283b44da2a80a1635527a9acf59b8f1782767
buitienthanhnt@gmail.com <namtanvietnam>
2023-08-28 10:03:47 +0700

    main/main-build: Update code for format.

--- bottoms/tabs/PaperScreen.js
+++ bottoms/tabs/PaperScreen.js
@@ -16,32 +16,32 @@ const PaperScreen = (props) => {
 
     return (
         <Stack.Navigator>
-            {/* <Stack.Screen name="PaperList" component={PaperList} options={{ headerShown: false }} /> */}
-            <Stack.Screen name="Sdetail" component={Sdetail}
-                options={{ // custom header of page xem: https://reactnavigation.org/docs/header-buttons && https://reactnavigation.org/docs/headers
-                    headerShown: true,
-                    title: "Bekijk Winkelvoorraad",
-                    headerTitleAlign: 'center',
-                    headerStyle: {
-                        backgroundColor: '#939393',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                    headerLeft: () => (
-                        <FontAwesome5Icon name={'chevron-left'} size={22} color='#ffffff' onPress={()=>{
-                            back();
-                        }} />
-                      ),
-                }}
-            />
             <Stack.Screen name="PaperList" component={PaperList} options={{ headerShown: false }} />
             <Stack.Screen name="PaperDetail" component={PaperDetail} options={{ headerShown: true }} />
             <Stack.Screen name="PaperListCategory" component={PaperListCategory} options={{ headerShown: false }} />
             <Stack.Screen name="Paper" component={Paper} options={{ headerShown: false }} />
             <Stack.Screen name="Detail" component={Detail} options={{ headerShown: true }} />
             <Stack.Screen name="WebInApp" component={WebInApp} options={{ headerShown: false }} />
+            {/* <Stack.Screen name="PaperList" component={PaperList} options={{ headerShown: false }} /> */}
+            <Stack.Screen name="Sdetail" component={Sdetail}
+                          options={{ // custom header of page xem: https://reactnavigation.org/docs/header-buttons && https://reactnavigation.org/docs/headers
+                              headerShown: true,
+                              title: "Bekijk Winkelvoorraad",
+                              headerTitleAlign: 'center',
+                              headerStyle: {
+                                  backgroundColor: '#939393',
+                              },
+                              headerTintColor: '#fff',
+                              headerTitleStyle: {
+                                  fontWeight: 'bold',
+                              },
+                              headerLeft: () => (
+                                  <FontAwesome5Icon name={'chevron-left'} size={22} color='#ffffff' onPress={()=>{
+                                      back();
+                                  }} />
+                              ),
+                          }}
+            />
         </Stack.Navigator>
     );
 };
