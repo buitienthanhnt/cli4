b7841767003e932d03a83b8dd9c1d7aeae413b13
thanhnt <buitienthanhnt@gmail.com>
2023-10-04 23:10:58 +0700

    messaging: up code

--- src/utils/messaging.js
+++ src/utils/messaging.js
@@ -1,5 +1,5 @@
-import messaging from "@react-native-firebase/messaging"; // lưu ý cần bật tính năng firebase push notification trên firebase.
-import {Linking} from 'react-native';
+import messaging from "@react-native-firebase/messaging"; // lưu ý cần bật tính năng firebase push notification trên firebase. test in: https://testfcm.com/
+import { Linking } from 'react-native';
 // import inAppMessaging from '@react-native-firebase/in-app-messaging';
 // import analytics from '@react-native-firebase/analytics';
 
@@ -10,6 +10,24 @@ messaging().setBackgroundMessageHandler(async message => {
 
 messaging().onNotificationOpenedApp(messaging => {
 	// mở app và điều hướng vào screen nào đó
+	// {
+	// 	"collapseKey": "com.cli4",
+	// 	"data": {
+	// 		"pid": "123"
+	// 	},
+	// 	"from": "515691323092",
+	// 	"messageId": "0:1696262980305161%d89f2361d89f2361",
+	// 	"notification": {
+	// 		"android": {
+	// 			"imageUrl": "https://cdnstoremedia.com/adt/adn/2017/01/anh2-adx5886b6d7ca7ac.jpg"
+	// 		},
+	// 		"body": "ooo",
+	// 		"title": "ppp"
+	// 	},
+	// 	"sentTime": 1696262728039,
+	// 	"ttl": 2419200
+	// }
+	console.log(messaging);
 	console.log(messaging.data.screen, `myapp://app/${messaging.data.screen}`);
 	Linking.openURL(`myapp://app/${messaging.data.screen}`);
 })
