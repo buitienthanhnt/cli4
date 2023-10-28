a9c2c52535dfa9214c6cc9649e39f8ec2513ea3b
thanhnt <buitienthanhnt@gmail.com>
2023-10-28 17:43:40 +0700

    main: Up code for use notifee push message display mesage and open mesage.

--- src/utils/messaging.js
+++ src/utils/messaging.js
@@ -1,24 +1,41 @@
 import messaging from "@react-native-firebase/messaging"; // lưu ý cần bật tính năng firebase push notification trên firebase. test in: https://testfcm.com/
 import { Linking } from 'react-native';
-import notifee from '@notifee/react-native'; // https://notifee.app/react-native/docs/installation
+import notifee, {AndroidStyle, EventType} from '@notifee/react-native'; // https://notifee.app/react-native/docs/installation
 
 // import inAppMessaging from '@react-native-firebase/in-app-messaging';
 // import analytics from '@react-native-firebase/analytics';
 
+// khi có tin nhắn tới khi app chạy nền.
 messaging().setBackgroundMessageHandler(async message => {
 	// navigation to screen
-	console.log(message);
+	console.log('_____', message);
+});
+
+// khi có tin nhắn tới app chạy nền
+notifee.onBackgroundEvent(async event => {
+	// hành động khi mở tin nhắn
+	if (event.type == EventType.PRESS) {
+		Linking.openURL(`myapp://app/${event.detail.notification.data.screen}`);
+	}
+});	
+
+// khi có tin nhắn tới trong app
+notifee.onForegroundEvent((event)=>{
+	// hành động khi mở tin nhắn
+	if (event.type == EventType.PRESS) {
+		Linking.openURL(`myapp://app/${event.detail.notification.data.screen}`);
+	}
 });
 
 // chạy khi có thông báo gửi tới, kể  cả đang trong app(thường dùng cho: Notifee - React Native)
 messaging().onMessage((message) => {
-	console.log(message);
-	onDisplayNotification(); // push notification by: notifee
+	onDisplayNotification(message); // push notification by: notifee
 });
 
-async function onDisplayNotification() {
+async function onDisplayNotification(message) {
+
 	// Request permissions (required for iOS)
-	await notifee.requestPermission()
+	// await notifee.requestPermission();
 
 	// Create a channel (required for Android)
 	const channelId = await notifee.createChannel({
@@ -27,18 +44,21 @@ async function onDisplayNotification() {
 	});
 
 	// Display a notification
+	const {data, notification} = message;
 	await notifee.displayNotification({
-		title: 'Notification Title',
-		body: 'Main body content of the notification',
+		title: notification.title,
+		body: notification.body,
 		android: {
 			channelId,
-			smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+			smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
 			// https://topdev.vn/blog/cach-tao-icon-tuong-thich-tren-android-bang-android-studio/
 			// pressAction is needed if you want the notification to open the app when pressed
 			pressAction: {
 				id: 'default',
 			},
+			style: { type: AndroidStyle.BIGPICTURE, picture: notification.android.imageUrl } // hình ảnh trên android
 		},
+		data: data
 	});
 }
 
