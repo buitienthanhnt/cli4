81dc2641b2909fcc55df38ef37e518744d5ce034
thanhnt <buitienthanhnt@gmail.com>
2023-11-03 12:42:37 +0700

    firebase: update for get data from cloud function from firebase store

--- screens/AccountScreen/FireStore.js
+++ screens/AccountScreen/FireStore.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
+import functions, { firebase } from '@react-native-firebase/functions';
 
-const FireStore = ()=>{
+const FireStore = () => {
 
 	const usersCollection = firestore().collection('demo1');
 	const reference = storage().ref('/m4700/-3886-1666684596.jpg');
 
-	const getData = async ()=>{
+	const getData = async () => {
 
 		const reference = await storage().ref('/m4700/-3886-1666684596.jpg').getDownloadURL(); // lay url anh view.
 
@@ -19,14 +20,29 @@ const FireStore = ()=>{
 		// console.log(users, '____', user);
 	}
 
-	useEffect(()=>{
-		getData();
+	useEffect(() => {
+		firebase.functions().useEmulator('localhost', 5001);
+		// getData();
 	}, []);
 
-	return(
-		<View>
+	return (
+		<View style={{padding: 10,}}>
 			<Text>react native firebase store</Text>
 			{/* <Image source={{uri: reference}} style={{width: 120, height: 120}}></Image> */}
+			<TouchableOpacity 
+				style={{backgroundColor: 'rgba(70, 0, 0, 0.7)', borderRadius: 6, height: 24, alignSelf: 'flex-start'}} 
+				onPress={async () => {
+					functions()
+					.httpsCallable('someMethod')({ abc: 123 })
+					.then(response => {
+						console.log('++++', response.data);
+					});
+					// const usersCollection = await firestore().collection('listUser').doc('u1@gmail.com').get();
+					// console.log('______', usersCollection);
+				}}
+			>
+				<Text>data by function from store</Text>
+			</TouchableOpacity>
 		</View>
 	)
 };
