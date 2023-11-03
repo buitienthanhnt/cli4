67818e8062d941edd6fa3169f5fb64339c9b2238
thanhnt <buitienthanhnt@gmail.com>
2023-11-03 15:22:17 +0700

    firebase-auth: Update code

--- screens/AccountScreen/FireStore.js
+++ screens/AccountScreen/FireStore.js
@@ -10,6 +10,54 @@ const FireStore = () => {
 	const usersCollection = firestore().collection('demo1');
 	const reference = storage().ref('/m4700/-3886-1666684596.jpg');
 
+	const addData = () => {
+		// add with auto key
+		firestore()
+			.collection('Users')
+			.add({
+				name: 'Ada Lovelace',
+				age: 30,
+			})
+			.then(() => {
+				console.log('User added!');
+			});
+
+		// add witd custom key
+		firestore()
+			.collection('Users')
+			.doc('ABC')
+			.set({
+				name: 'Ada Lovelace',
+				age: 30,
+			})
+			.then(() => {
+				console.log('User added!');
+			});
+	};
+
+	// update for doc
+	const update = () => {
+		firestore()
+			.collection('Users')
+			.doc('ABC')
+			.update({
+				age: 31,
+			})
+			.then(() => {
+				console.log('User updated!');
+			});
+	};
+
+	const deleteData = () => {
+		firestore()
+			.collection('Users')
+			.doc('ABC')
+			.delete()
+			.then(() => {
+				console.log('User deleted!');
+			});
+	}
+
 	const getData = async () => {
 
 		const reference = await storage().ref('/m4700/-3886-1666684596.jpg').getDownloadURL(); // lay url anh view.
@@ -26,22 +74,38 @@ const FireStore = () => {
 	}, []);
 
 	return (
-		<View style={{padding: 10,}}>
+		<View style={{ padding: 10, }}>
 			<Text>react native firebase store</Text>
 			{/* <Image source={{uri: reference}} style={{width: 120, height: 120}}></Image> */}
-			<TouchableOpacity 
-				style={{backgroundColor: 'rgba(70, 0, 0, 0.7)', borderRadius: 6, height: 24, alignSelf: 'flex-start'}} 
+			<TouchableOpacity
+				style={{ backgroundColor: 'rgba(70, 0, 0, 0.7)', borderRadius: 6, height: 24, alignSelf: 'flex-start' }}
 				onPress={async () => {
 					functions()
-					.httpsCallable('someMethod')({ abc: 123 })
-					.then(response => {
-						console.log('++++', response.data);
-					});
+						.httpsCallable('someMethod')({ abc: 123 })
+						.then(response => {
+							console.log('++++', response.data);
+						});
 					// const usersCollection = await firestore().collection('listUser').doc('u1@gmail.com').get();
 					// console.log('______', usersCollection);
 				}}
 			>
 				<Text>data by function from store</Text>
+			</TouchableOpacity>
+			<Text></Text>
+
+			<TouchableOpacity
+				style={{ backgroundColor: 'rgba(70, 0, 0, 0.7)', borderRadius: 6, height: 24, alignSelf: 'flex-start' }}
+				onPress={async () => {
+					functions()
+						.httpsCallable('sendHttpPushNotification')({ abc: 123 })
+						.then(response => {
+							console.log('++++', response.data);
+						});
+					// const usersCollection = await firestore().collection('listUser').doc('u1@gmail.com').get();
+					// console.log('______', usersCollection);
+				}}
+			>
+				<Text>push message</Text>
 			</TouchableOpacity>
 		</View>
 	)
