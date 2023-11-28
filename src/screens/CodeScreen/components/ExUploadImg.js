eb60d51532c38b14bc5ced9607a44ba10f452000
thanhnt <buitienthanhnt@gmail.com>
2023-11-28 23:03:03 +0700

    update code

--- src/screens/CodeScreen/components/ExUploadImg.js
+++ src/screens/CodeScreen/components/ExUploadImg.js
@@ -1,6 +1,6 @@
-import {useCallback, useState} from 'react';
+import { useCallback, useState } from 'react';
 import { Button, Dimensions, Text, TouchableOpacity, View, Image, ScrollView } from "react-native";
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker'; // https://github.com/react-native-image-picker/react-native-image-picker
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'; // https://github.com/react-native-image-picker/react-native-image-picker
 import Config from '@config/Config';
 import axios from 'axios';
 import { utils } from '@react-native-firebase/app';
@@ -9,13 +9,13 @@ import storage from '@react-native-firebase/storage';
 import "react-native-get-random-values"; // yarn add react-native-get-random-values đi kèm với uuid nếu không uuid sẽ có thể bị lỗi.
 import { v4 as uuidv4 } from 'uuid';     // yarn add uuid https://github.com/uuidjs/uuid hoặc: yarn add @types/uuid -D (cho typeScript)
 
-const ExUploadImg = ()=>{
+const ExUploadImg = () => {
     const [image, setImage] = useState([]);
 
     /**
      * chọn ảnh từ thư viện ảnh trên mobile
      */
-    const pickImage = async () => {
+    const pickImage = useCallback(async () => {
         // No permissions request is necessary for launching the image library
         let result = await launchImageLibrary({
             mediaTypes: 'photo',
@@ -38,12 +38,12 @@ const ExUploadImg = ()=>{
 
         if (!result.didCancel) {
             const target_image = [];
-            result?.assets.map((imageItem, index)=>{
+            result?.assets.map((imageItem, index) => {
                 target_image.push(imageItem?.uri);
             })
             setImage(target_image);
         }
-    };
+    }, []);
 
     /**
      * upload image to laravel server
@@ -60,7 +60,7 @@ const ExUploadImg = ()=>{
 
             const config = {
                 method: "post",
-                url: Config.custom_url()+Config.api_request.uploadImageMb,
+                url: Config.custom_url() + Config.api_request.uploadImageMb,
                 responseType: "json",
                 headers: {
                     'Content-Type': 'multipart/form-data',
@@ -105,29 +105,29 @@ const ExUploadImg = ()=>{
         let img = '';
         const imageName = uuidv4();
         // tham chiếu tới đối tượng ảnh chuẩn bị upload.
-        const reference = storage().ref('/demo/'+ imageName);
+        const reference = storage().ref('/demo/' + imageName);
         // uploads file
         await reference.putFile(image_path);
         // task = reference.putFile(image_path);
         // task.on('state_changed', taskSnapshot => {
-            // console.log('___taskSnapshot', taskSnapshot.metadata);
-            // "metadata": {
-            //     "bucket": "react-cli4.appspot.com", "contentDisposition": "inline; filename*=utf-8''d69f0a14-9700-4ecb-a6c4-131be88edd7a", 
-            //     "contentEncoding": "identity", "contentType": "image/png", 
-            //     "fullPath": "demo/d69f0a14-9700-4ecb-a6c4-131be88edd7a", "generation": "1701145165411082", 
-            //     "md5Hash": "l7jCVfY7tO0eWTpXgr61zQ==", "metageneration": "1", 
-            //     "name": "d69f0a14-9700-4ecb-a6c4-131be88edd7a", 
-            //     "size": 155204, "timeCreated": "2023-11-28T04:19:25Z", 
-            //     "updated": "2023-11-28T04:19:25Z"}, 
-            //     "state": "success", 
-            //     "totalBytes": 155204
-            // }
+        // console.log('___taskSnapshot', taskSnapshot.metadata);
+        // "metadata": {
+        //     "bucket": "react-cli4.appspot.com", "contentDisposition": "inline; filename*=utf-8''d69f0a14-9700-4ecb-a6c4-131be88edd7a", 
+        //     "contentEncoding": "identity", "contentType": "image/png", 
+        //     "fullPath": "demo/d69f0a14-9700-4ecb-a6c4-131be88edd7a", "generation": "1701145165411082", 
+        //     "md5Hash": "l7jCVfY7tO0eWTpXgr61zQ==", "metageneration": "1", 
+        //     "name": "d69f0a14-9700-4ecb-a6c4-131be88edd7a", 
+        //     "size": 155204, "timeCreated": "2023-11-28T04:19:25Z", 
+        //     "updated": "2023-11-28T04:19:25Z"}, 
+        //     "state": "success", 
+        //     "totalBytes": 155204
+        // }
         // });
         // task.then(async (onFulfilled, onRejected) => {
-            // lấy link của ảnh sau upload:
-            // https://firebasestorage.googleapis.com/v0/b/react-cli4.appspot.com/o/demo%2F552360c8-7ab5-4d63-a196-3d4505b4dcaf?alt=media&token=ec925e45-00e1-4984-b61a-f0deb177155a
-            // const image_url = await reference.getDownloadURL();
-            // console.log('', onFulfilled, image_url);
+        // lấy link của ảnh sau upload:
+        // https://firebasestorage.googleapis.com/v0/b/react-cli4.appspot.com/o/demo%2F552360c8-7ab5-4d63-a196-3d4505b4dcaf?alt=media&token=ec925e45-00e1-4984-b61a-f0deb177155a
+        // const image_url = await reference.getDownloadURL();
+        // console.log('', onFulfilled, image_url);
         // });
         // lấy đường dẫn ảnh https uri
         img = await reference.getDownloadURL();
@@ -137,7 +137,7 @@ const ExUploadImg = ()=>{
     /**
      * upload multi image into storege firebase.
      */
-    const storageUploadImages = useCallback(async ()=>{
+    const storageUploadImages = useCallback(async () => {
         let resultData = [];
         if (image.length) {
             // không dùng với map:
@@ -153,7 +153,7 @@ const ExUploadImg = ()=>{
                 if (res_uri) {
                     resultData.push({
                         url: res_uri,
-                        meta: {type: ''}
+                        meta: { type: '' }
                     });
                 }
             }
@@ -164,54 +164,64 @@ const ExUploadImg = ()=>{
         return [];
     }, []);
 
-    return(
-        <View>
-            <View style={{gap: 20}}>
-                <Text style={{color: '#ff8cf9', fontSize: 18, fontWeight: '500'}}>select image for upload: </Text>
-                <Button title="Pick an image from camera roll" onPress={pickImage} />
-                <ScrollView 
-                    horizontal={true} 
+    return (
+        <View style={{ padding: 10 }}>
+            <View style={{ gap: 20 }}>
+                <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity
+                        style={{
+                            backgroundColor: 'rgba(148,74,228,0.8)', height: 32, justifyContent: 'center',
+                            alignItems: 'center', width: 220, borderRadius: 40
+                        }}
+                        onPress={pickImage}
+                    >
+                        <Text>Pick an image from camera</Text>
+                    </TouchableOpacity>
+                </View>
+                <ScrollView
+                    horizontal={true}
                     // pagingEnabled={true} 
-                    style={{height: 160,}} 
+                    style={{ height: 160, }}
                     showsHorizontalScrollIndicator={false}
                 >
-                    {image.length && image.map((item, index)=>{
-                        return(
-                            <View>
-                                <Image 
-                                source={{ uri: item}} 
-                                style={{ width: 120, height: 120, marginHorizontal: 8 }} 
-                                borderRadius={16}
-                                key={'image_'+index} resizeMode='cover'/>
-                                <TouchableOpacity style={{position: 'absolute', left: 20, top: 5}} 
-                                    onPress={()=>{
+                    {image.map((item, index) => {
+                        return (
+                            <View key={'imageT_' + index}>
+                                <Image
+                                    source={{ uri: item }}
+                                    style={{ width: 120, height: 120, marginHorizontal: 8 }}
+                                    borderRadius={16}
+                                    key={'image_' + index} resizeMode='cover' />
+                                <TouchableOpacity style={{ position: 'absolute', left: 20, top: 5 }}
+                                    onPress={() => {
+                                        console.log(index);
                                         let target_image = image;
                                         target_image.splice(index, 1);
                                         setImage([...target_image]);
                                     }}>
-                                    <Text style={{fontSize: 16, fontWeight: 'bold'}}>X</Text>
+                                    <Text style={{ fontSize: 16, fontWeight: 'bold' }}>X</Text>
                                 </TouchableOpacity>
                             </View>
                         )
                     })}
                 </ScrollView>
             </View>
-            <View style={{ backgroundColor: "black", height: 1,marginVertical: 10 }}></View>
+            <View style={{ backgroundColor: "black", height: 1, marginVertical: 10 }}></View>
 
             {/* ========================upload into laravel server================================== */}
             <View>
-                <Text style={{color: '#ff8cf9', fontSize: 18, fontWeight: '500'}}>for laravel services: </Text>
+                <Text style={{ color: '#ff8cf9', fontSize: 18, fontWeight: '500' }}>for laravel services: </Text>
                 <Button title="submit" onPress={() => {
-                    up_load_image(image);
+                    up_load_image(image[0]);
                 }}></Button>
             </View>
             {/* ========================upload in to laravel server================================== */}
-            <View style={{ backgroundColor: "black", height: 1,marginVertical: 10 }}></View>
+            <View style={{ backgroundColor: "black", height: 1, marginVertical: 10 }}></View>
             {/* ========================upload into storage firebase================================== */}
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{color: '#9389ff', fontSize: 18, fontWeight: '500'}}>for firebase application: </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: '#9389ff', fontSize: 18, fontWeight: '500' }}>for firebase application: </Text>
                 <TouchableOpacity
-                    style={{backgroundColor: '#afffe0', height: 32, justifyContent: 'center', alignItems: 'center', width: 160, borderRadius: 40}}
+                    style={{ backgroundColor: '#afffe0', height: 32, justifyContent: 'center', alignItems: 'center', width: 160, borderRadius: 40 }}
                     onPress={async () => {
                         storageUploadImages();
                     }}
